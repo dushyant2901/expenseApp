e5f91225c5a7661d50c2599df049bdba49b5a846
Dushyant Pratap Singh <singhdushyant2901@gmail.com>
2022-10-03 13:15:08 +0530

    added clear and hide  functionality

--- app.js
+++ app.js
@@ -1,53 +1,63 @@
-const billAmount = document.querySelector("#bill-Amount")
-const cashGiven = document.querySelector("#cash-Given")
-const checkBtn = document.querySelector("#check-Btn")
-const message = document.querySelector("#message")
-const noteCount = document.querySelectorAll(".noteCount")
-const notes = [2000, 500, 100, 20, 10, 5, 1]
+const billAmount = document.querySelector("#bill-Amount");
+const cashGiven = document.querySelector("#cash-Given");
+const checkBtn = document.querySelector("#check-Btn");
+const clearBtn = document.querySelector("#clear-Btn");
+const message = document.querySelector("#message");
+const noteCount = document.querySelectorAll(".noteCount");
+const table = document.querySelector(".table");
+const notes = [2000, 500, 100, 20, 10, 5, 1];
 
-checkBtn.addEventListener("click", validateAmount)
+checkBtn.addEventListener("click", validateAmount);
 
 function validateAmount() {
-    hideMessage()
-
-    if (billAmount.value > 0) {
-        if (cashGiven.value >= billAmount.value) {
-
-            let changeAmount = cashGiven.value - billAmount.value;
-            console.log("kkkkkkkkkkkkkkkkkk")
-            calculateNotes(changeAmount);
-        }
-        else {
-            console.log("yo")
-            showMessage("Cash given should be greater than or at least equal to bill amount")
-        }
-
-
-
-
+  hideElem(message);
+  const billAmountVal = convertStrToNum(billAmount.value);
+  const cashGivenVal = convertStrToNum(cashGiven.value);
+  console.log(billAmountVal, cashGivenVal);
+  if (billAmountVal > 0) {
+    if (cashGivenVal >= billAmountVal) {
+      let changeAmount = cashGivenVal - billAmountVal;
+      if (changeAmount) {
+        calculateNotes(changeAmount);
+        showElem(table);
+      }
     } else {
-        showMessage("The bill amount should be greater than 0")
+      showMessage(
+        "Cash given should be greater than or at least equal to bill amount"
+      );
+      showElem(message);
     }
-
-
+  } else {
+    showMessage("The bill amount should be greater than 0");
+    showElem(message);
+  }
 }
 
 function showMessage(msg) {
-    message.style.display = "block"
-    message.innerHTML = msg
+  message.innerHTML = msg;
 }
 
-function hideMessage() {
-    message.style.display = "none"
-
+function hideElem(elem) {
+  elem.style.display = "none";
+}
+function showElem(elem) {
+  elem.style.display = "block";
 }
 
 function calculateNotes(amount) {
-    for (let i = 0; i < notes.length; i++) {
-        const numberOfNotes = Math.trunc(amount / notes[i])
-        amount = amount % notes[i]
-        noteCount[i].innerText = numberOfNotes
-    }
-
-
+  for (let i = 0; i < notes.length; i++) {
+    const numberOfNotes = Math.trunc(amount / notes[i]);
+    amount = amount % notes[i];
+    noteCount[i].innerText = numberOfNotes;
+  }
 }
+function convertStrToNum(val) {
+  return Number(val);
+}
+clearBtn.addEventListener("click", () => {
+  console.log("first");
+  billAmount.value = "";
+  cashGiven.value = "";
+  noteCount.forEach((count) => (count.innerText = ""));
+  hideElem(table);
+});
